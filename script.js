2f7c5ffd735952cd7179863533f9ef8a8cbe2673
A-Ghoujal <abdogohjal@gmail.com>
2024-09-18 12:27:31 +0100

    emailjs setup

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 const image = document.querySelector('.influencer-image');
 
+// Initialize EmailJS
+(function(){
+    emailjs.init("kSALH80pqaVYUPIfg"); // Replace with your EmailJS user ID
+ })();
+ 
+ // Add event listener for form submission
+ document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+ 
+    // Send form data using EmailJS
+    emailjs.sendForm('service_sfi1lug', 'template_z0tlybk', this)
+    .then(function() {
+        alert('Message sent successfully!');
+    }, function(error) {
+        alert('Failed to send message: ' + error.text);
+    });
+ });
+ 
